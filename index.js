ca46755b21f659ce834062fbe7bcc21b0692e4dd
Dmitriy Petrukhin <d.a.blissful@gmail.com>
2022-04-03 17:43:55 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const TelegramApi = require('node-telegram-bot-api')
+const token = process.env.API_KEY;
+const bot = new TelegramApi(token, {polling: true})
+const delMsg = () => {
+    bot.on('message', async (msg) => {
+        if (msg.voice) {
+            const chatId = msg.chat.id
+            const messageId = msg.message_id
+            const name = msg.from.first_name
+            await bot.deleteMessage(chatId, messageId)
+            await bot.sendMessage(chatId, `${name}, аудиосообщения запрещены в чате`)
+        }
+    })
+}
+delMsg();
